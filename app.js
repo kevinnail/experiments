7574a87fdea320504d57b9b6b8817af05767a687
kevinnail <fumalicious@gmail.com>
2022-08-12 11:29:40 -0700

    add local directory to repo

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
+const myButton = document.getElementById("button1");
+
+myButton.addEventListener("click", () => {
+  console.log("My javascript is working.");
+ myButton.classList.toggle("pushed");
+
+});
 
 
 
 
+// soda.addEventListener("click", () => {
+//   console.log("clicking");
+//   soda.classList.toggle("picked");
+//   if (soda.classList.contains("picked")) {
+//     numberOfItems = numberOfItems + 1;
+//   } else {
+//     numberOfItems = numberOfItems - 1;
+//   }
 
-
-
-
-
+//   items.textContent = " " + numberOfItems;
+// });

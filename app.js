e3b0908c3a3d03e430fa7c2cc27637491c752405
kevinnail <fumalicious@gmail.com>
2022-08-12 12:17:04 -0700

    update so first exercise from book works when button is clicked on

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
 const myButton = document.getElementById("button1");
+const unPush = myButton.classList.toggle("pushed");
+
+// const showText () => {
+//   (for (let line = "#"; line.length < 8; line += "#")
+//   console.log(line))};
+
+
 
 myButton.addEventListener("click", () => {
-  console.log("My javascript is working.");
- myButton.classList.toggle("pushed");
-
+  // console.log(showText);
+  myButton.classList.toggle("pushed");
+  // myButton.setTimeout(unPush, 1000);
+  doThis();
 });
 
 
+function doThis() {
+  for (let line = "#"; line.length < 8; line += "#")
+  console.log(line);
+}
 
 
-// soda.addEventListener("click", () => {
-//   console.log("clicking");
-//   soda.classList.toggle("picked");
-//   if (soda.classList.contains("picked")) {
-//     numberOfItems = numberOfItems + 1;
-//   } else {
-//     numberOfItems = numberOfItems - 1;
-//   }
 
-//   items.textContent = " " + numberOfItems;
-// });
+// function handleSubmit() {
+//   // questionContainer.classList.toggle('hide');
+//   questionContainer.classList.add("fade-out");
+//   fortuneContainer.classList.toggle("hide");
+//   fortuneContainer.classList.add("fade-in");
+//   const randNum = Math.floor(Math.random() * answers.length);
+//   const randomAnswer = answers[randNum];
+//   console.log(randomAnswer);
+//   result.textContent = randomAnswer;
+// }
